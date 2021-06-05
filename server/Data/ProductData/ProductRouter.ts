1e0c4c92ee73a944930bea1fd8377bae81fd63aa
Ivan <Ivan.Aimufua@atco.com>
2021-06-05 11:20:17 -0600

    recent changes to project

---
+++ server/Data/ProductData/ProductRouter.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { getConnection } from "typeorm";
+import { Product } from './ProductDTO';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try{
+        let productResponseData = [] as Product[];
+        const products = await getConnection().manager.getRepository(Product).find();
+        if (products.length > 0) {
+            productResponseData = products;
+        }
+        res.status(200).json(productResponseData);
+    } catch(error) {
+        res.status(400).send(`error something went wrong error: ${JSON.stringify(error)}`);
+    }
+});
+
+router.get('/:productId', async (req, res) => {
+    try {
+        let product = {} as Product;
+        const queriedProduct = await getConnection().manager.getRepository(Product).findOne({ where: { Id: req.params.productId }}) as Product;
+
+        if (queriedProduct !== undefined) {
+            product = { ...queriedProduct };
+        }
+        res.status(200).json(product)
+    } catch(error) {
+        res.status(400).send(`error something went wrong error: ${JSON.stringify(error)}`);
+    }
+})
+
+export default router;
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,31 @@
 import express from 'express';
-
+import "reflect-metadata";
+import * as bodyParser from "body-parser";
+import { createConnection } from "typeorm";
+import { Product } from './Data/ProductData/ProductDTO';
+import ProductRouter from './Data/ProductData/ProductRouter';
 const app = express();
+const main = express();
+
+// main.use("/api/v1", app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({extended: false}));
+
 const PORT = 8000;
 
-app.get('/', (req, res) => res.send('Express + Typescript Server'));
+app.use('/api/products', ProductRouter);
 
 app.listen(PORT, () => {
-    console.log(`[server]: Server running at https://localhost:${PORT}`);
-})
+  createConnection({
+      type: "mssql",
+      port: 1433,
+      host: "ecommercerumi.database.windows.net",
+      username: "ivanaimufua41@gmail.com@ecommercerumi",
+      password: "24Kingswood@",
+      database: "Ecommerce-Test",
+      entities: [Product],
+    }).then(() => {
+      console.log('Connected to database successfully!');
+      console.log(`[server]: Server running at https://localhost:${PORT}`);
+    });
+});
